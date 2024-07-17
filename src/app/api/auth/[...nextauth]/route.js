3c71da1a44db16935b651f259c7fe685ed69d3f8
Yimi-17 <54036463+Yimi-17@users.noreply.github.com>
2024-07-17 09:38:43 -0500

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import db from '../../../../libs/db'
+import db from '../../../../libs/db';
 import bcrypt from 'bcrypt';
-
 
 export const authOptions = {
   providers: [
@@ -13,22 +12,27 @@ export const authOptions = {
         password: { label: "Password", type: "password", placeholder: "*****" },
       },
       async authorize(credentials, req) {
-        const userFound = await db.user.findUnique({
-          where: {
-            email: credentials.email
+        try {
+          const userFound = await db.user.findUnique({
+            where: {
+              email: credentials.email
+            }
+          });
+
+          if (!userFound) throw new Error('No user found');
+
+          const matchPassword = await bcrypt.compare(credentials.password, userFound.password);
+
+          if (!matchPassword) throw new Error('Wrong password');
+
+          return {
+            id: userFound.id,
+            name: userFound.username,
+            email: userFound.email,
           }
-        });
-
-        if (!userFound) throw new Error('No user found');
-
-        const matchPassword = await bcrypt.compare(credentials.password, userFound.password);
-
-        if (!matchPassword) throw new Error('Wrong password');
-
-        return {
-          id: userFound.id,
-          name: userFound.username,
-          email: userFound.email,
+        } catch (error) {
+          console.error('Authorization error:', error);
+          throw new Error('Authorization failed');
         }
       },
     }),
